8bae52b4f2e3149b050bb5c3aca39c922fc1cf4f
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-04-11 00:30:38 +0530

    Displaying data-curly braces

---
+++ React/App.js
@@ -0,0 +1,25 @@
+//JSX lets you put markup into JavaScript. Curly braces let you “escape back” into JavaScript 
+//so that you can embed some variable from your code and display it to the user. For example, this will display user.name:
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://react.dev/images/docs/scientists/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
